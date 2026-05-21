df330255eeb8dce569c42b1224ea00003e348c54
iisalut <tulasiin@gmail.com>
2026-05-21 18:03:06 -0400

    set up all question types + implemented random genration for temperature question type

---
+++ actual-le-chateliers/game_manager.gd
@@ -0,0 +1,39 @@
+extends Control
+
+var random_select_temp= randi_range(0,5)
+@onready var question_label: RichTextLabel = $question_label
+@onready var button: Button = $Button
+@onready var enthalpy_label: RichTextLabel = $enthalpy_label
+var curr_qs=""
+var curr_enthalpy=""
+func _ready():
+	choose_temp_qs()
+	question_label.text= curr_qs
+	enthalpy_label.text=curr_enthalpy
+
+func choose_temp_qs():
+	# already starts from the a random value and then 
+	#goes into the actual rxn : enthalpy -> dict always loops through keys 
+	
+	var inner_dict=QsData.temp_qs_dict[random_select_temp]
+	for rxn in inner_dict :
+		curr_qs= rxn
+		curr_enthalpy=inner_dict[rxn]
+			
+
+func final_temp():
+	random_select_temp= randi_range(0,5)
+	choose_temp_qs()
+	question_label.text= curr_qs
+	enthalpy_label.text=curr_enthalpy
+	
+
+func _on_button_pressed() -> void:
+	enthalpy_label.text=""
+	var random_of_3= randi_range(1,3)
+	if (random_of_3== 1):
+		final_temp()
+	elif (random_of_3==2):
+		question_label.text="volume qs"
+	else:
+		question_label.text="conc qs"
---
+++ actual-le-chateliers/qs_data.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var temp_qs_dict={ 0: {"CH4 + 2 O2 --> CO2 + 2 H2O" :"exothermic"},
+1:{"HCl + NaOH --> NaCl + H2O": "exothermic"},
+2:{"C6H12O6 +6 O2 --> 6 CO2 + 6 H2O" :"exothermic"},
+3:{"6 CO2 + 6 H2O --> C6H12O6 + 6 O2" : "endothermic"},
+4:{"CaCO3 --> CaO + CO2" : "endothermic"},
+5:{"NH4NO3 --> NH4+ + NO3-" : "endothermic"}
+}
